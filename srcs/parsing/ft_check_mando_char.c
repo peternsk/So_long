6e2318d4b1d33a9d060c4e215cb465ea3a09dfa8
peternsk <petermalonge@gmail.com>
2023-10-16 17:35:51 -0400

    done

--- srcs/parsing/ft_check_mando_char.c
+++ srcs/parsing/ft_check_mando_char.c
@@ -6,7 +6,7 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/11 21:07:13 by peternsaka        #+#    #+#             */
-/*   Updated: 2023/10/13 15:55:59 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/10/16 17:23:25 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,10 @@ bool	ft_check_mando_char(t_game_map *game)
 	num_of_player = game->player;
 	num_of_exit = game->way_out;
 	num_of_collect = game->collect;
-	if (num_of_player != 1 && num_of_exit != 1 && num_of_collect <= 0)
+	printf("player %d\n", game->player);
+	printf("collect %d\n", game->collect);
+	printf("exit %d\n", game->way_out);
+	if (num_of_player != 1 || num_of_exit != 1 || num_of_collect <= 0)
 		return (false);
 	return (true);
 }
